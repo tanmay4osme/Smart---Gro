e80751ac75d69d97ac6768d280057964fe0fc940
Adit Bala <62257689+adit-bala@users.noreply.github.com>
2020-03-29 13:54:13 -0400

    Update MainPage.dart

--- MainPage.dart
+++ MainPage.dart
@@ -18,63 +18,79 @@ class App extends StatelessWidget {
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Gardening App Home Page'),
-        backgroundColor: Colors.purpleAccent,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Click button to move to SubPage'),
-            RaisedButton(
-              textColor: Colors.black,
-              color: Colors.green[200],
-              child: Text('Go to SubPage 1/Info/Getting Started'),
-              onPressed: () {
-                navigateToinfo(context);
-              },
-            ),
-            RaisedButton(
-                textColor: Colors.black,
-                color: Colors.green[200],
-                child: Text('Go to SubPage 2/Plants'),
-                onPressed: () {
-                  navigateToplants(context);
-                }
-            ),
-            RaisedButton(
-                textColor: Colors.black,
-                color: Colors.green[200],
-                child: Text('Go to SubPage 3/Connect'),
-                onPressed: () {
-                  navigateTosettings(context);
-                }
-            ),
-            RaisedButton(
-                textColor: Colors.black,
-                color: Colors.green[200],
-                child: Text('Go to Sub Page 4/Settings'),
-                onPressed: () {
-                  navigateToconnect(context);
-                }
-            )
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(
+        title: 'Welcome to Flutter',
+        home: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/image/Plant.jpg"),
+                    fit: BoxFit.cover)),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                title: Text('Gardening App Home Page'),
+                backgroundColor: Colors.purpleAccent,
+                leading: IconButton(
+                    icon: Icon(
+                      Icons.list,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {}),
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Click button to move to SubPage'),
+                    RaisedButton(
+                      textColor: Colors.black,
+                      color: Colors.green[200],
+                      child: Text('Getting Started'),
+                      onPressed: () {
+                        navigateToinfo(context);
+                      },
+                    ),
+                    RaisedButton(
+                        textColor: Colors.black,
+                        color: Colors.green[200],
+                        child: Text('Plants!'),
+                        onPressed: () {
+                          navigateToplants(context);
+                        }),
+                    RaisedButton(
+                        textColor: Colors.black,
+                        color: Colors.green[200],
+                        child: Text('Connect to Google'),
+                        onPressed: () {
+                          navigateTosettings(context);
+                        }),
+                    RaisedButton(
+                        textColor: Colors.black,
+                        color: Colors.green[200],
+                        child: Text('Settings'),
+                        onPressed: () {
+                          navigateToconnect(context);
+                        })
+                  ],
+                ),
+              ),
+            )));
   }
 
   Future navigateToinfo(context) async {
     Navigator.push(context, MaterialPageRoute(builder: (context) => info()));
   }
+
   Future navigateToplants(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => plants()));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => TableLayout()));
   }
+
   Future navigateTosettings(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => settings()));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => settings()));
   }
+
   Future navigateToconnect(context) async {
     Navigator.push(context, MaterialPageRoute(builder: (context) => connect()));
   }
