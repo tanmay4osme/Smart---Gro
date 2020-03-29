ba850fcb15c3346ab8c77cf8276bdee6f2b8c0c4
Ahila Moorthy <60198377+AhilaM@users.noreply.github.com>
2020-03-29 18:17:59 -0400

    Update reuse.dart

--- reuse.dart
+++ reuse.dart
@@ -17,19 +17,32 @@ class Reuse extends StatelessWidget{
         title: Text('Reusing Materials'),
         backgroundColor: Colors.brown,
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/image/Plant2.PNG"),
+                fit: BoxFit.cover)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('There are plenty of materials we use '
                 'in our daily lives that could be repurposed '
                 'for gardening instead of being thrown in '
-                'the trash.  \n\n'),
-            Text(tips, textAlign: TextAlign.justify),
+                'the trash.  \n\n',textAlign: TextAlign.justify,
+            style: TextStyle(
+                color: Colors.brown[800],
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0)),
+          Text(tips,
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+                color: Colors.brown[800],
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0)),
           ],
         ),
       ),
     );
   }
 }
-
