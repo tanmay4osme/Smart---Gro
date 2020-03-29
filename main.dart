7841da400603e4829bdd52a0cb43a0db5b548951
Ahila Moorthy <60198377+AhilaM@users.noreply.github.com>
2020-03-29 16:35:40 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,81 +1,224 @@
 import 'package:flutter/material.dart';
-import 'info.dart';
-import 'plants.dart';
-import 'settings.dart';
-import 'connect.dart';
-void main() => runApp(App());
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/cupertino.dart';
+import 'my_navigator.dart';
+import 'dart:async';
 
-class App extends StatelessWidget {
+void main() => runApp(new MaterialApp(
+  theme:
+  ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+  debugShowCheckedModeBanner: false,
+  home: SplashScreen(),
+));
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 5), () => MyNavigator.start(context));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.greenAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.local_florist,
+                          color: Colors.amberAccent,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Smart-Gro",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      "Welcome",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Navigation',
-      home: MainPage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MainPage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  new FlutterLocalNotificationsPlugin();
+  var initializationSettingsAndroid;
+  var initializationSettingsIOS;
+  var initializationSettings;
+
+  void _showNotification() async {
+    await _demoNotification();
+  }
+
+  Future<void> _demoNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'channel_ID', 'channel name', 'channel description',
+        importance: Importance.Max,
+        priority: Priority.High,
+        ticker: 'test ticker');
+
+    var iOSChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(0, 'Hello, buddy',
+        'A message from flutter buddy', platformChannelSpecifics,
+        payload: 'test oayload');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initializationSettingsAndroid =
+    new AndroidInitializationSettings('app_icon');
+    initializationSettingsIOS = new IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  Future onSelectNotification(String payload) async {
+    if (payload != null) {
+      debugPrint('Notification payload: $payload');
+    }
+    await Navigator.push(context,
+        new MaterialPageRoute(builder: (context) => new SecondRoute()));
+  }
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: Text(title),
+          content: Text(body),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              child: Text('Ok'),
+              onPressed: () async {
+                Navigator.of(context, rootNavigator: true).pop();
+                await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SecondRoute()));
+              },
+            )
+          ],
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gardening App Home Page'),
-        backgroundColor: Colors.brown,
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Click button to move to SubPage'),
-            RaisedButton(
-              textColor: Colors.black,
-              color: Colors.green[200],
-              child: Text('Go to SubPage 1/Info/Getting Started'),
-              onPressed: () {
-                navigateToinfo(context);
-              },
-            ),
-            RaisedButton(
-                textColor: Colors.black,
-                color: Colors.green[200],
-                child: Text('Go to SubPage 2/Plants'),
-                onPressed: () {
-                  navigateToplants(context);
-                }
-            ),
-            RaisedButton(
-                textColor: Colors.black,
-                color: Colors.green[200],
-                child: Text('Go to SubPage 3/Connect'),
-                onPressed: () {
-                  navigateTosettings(context);
-                }
-            ),
-            RaisedButton(
-                textColor: Colors.black,
-                color: Colors.green[200],
-                child: Text('Go to Sub Page 4/Settings'),
-                onPressed: () {
-                  navigateToconnect(context);
-                }
-            )
-          ],
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showNotification,
+        tooltip: 'Increment',
+        child: Icon(Icons.notifications),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AlertPage'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('go Back ...'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
       ),
     );
-  }
-
-  Future navigateToinfo(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => info()));
-  }
-  Future navigateToplants(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => plants()));
-  }
-  Future navigateTosettings(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => settings()));
-  }
-  Future navigateToconnect(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => connect()));
   }
 }
