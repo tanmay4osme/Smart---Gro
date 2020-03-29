b59c1b88693f279bec3e75c060390c54eb79ccb7
nivetham2000 <62526476+nivetham2000@users.noreply.github.com>
2020-03-29 13:13:36 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,90 +1,81 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'my_navigator.dart';
-void main() => runApp(new MaterialApp(
-  theme:
-    ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
-  debugShowCheckedModeBanner: false,
-  home: SplashScreen(),
-));
-class SplashScreen extends StatefulWidget {
+import 'info.dart';
+import 'plants.dart';
+import 'settings.dart';
+import 'connect.dart';
+void main() => runApp(App());
+
+class App extends StatelessWidget {
   @override
-  _SplashScreenState createState() => new _SplashScreenState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigation',
+      home: MainPage(),
+    );
+  }
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Timer(Duration(seconds: 5), () => MyNavigator.start(context));
-  }
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.greenAccent),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 50.0,
-                        child: Icon(
-                          Icons.local_florist,
-                          color: Colors.amberAccent,
-                          size: 50.0,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 10.0),
-                      ),
-                      Text(
-                        "Plant-Eco",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24.0),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                    ),
-                    Text(
-                      "Welcome",
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                          color: Colors.white),
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
+      appBar: AppBar(
+        title: Text('Gardening App Home Page'),
+        backgroundColor: Colors.brown,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Click button to move to SubPage'),
+            RaisedButton(
+              textColor: Colors.black,
+              color: Colors.green[200],
+              child: Text('Go to SubPage 1/Info/Getting Started'),
+              onPressed: () {
+                navigateToinfo(context);
+              },
+            ),
+            RaisedButton(
+                textColor: Colors.black,
+                color: Colors.green[200],
+                child: Text('Go to SubPage 2/Plants'),
+                onPressed: () {
+                  navigateToplants(context);
+                }
+            ),
+            RaisedButton(
+                textColor: Colors.black,
+                color: Colors.green[200],
+                child: Text('Go to SubPage 3/Connect'),
+                onPressed: () {
+                  navigateTosettings(context);
+                }
+            ),
+            RaisedButton(
+                textColor: Colors.black,
+                color: Colors.green[200],
+                child: Text('Go to Sub Page 4/Settings'),
+                onPressed: () {
+                  navigateToconnect(context);
+                }
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Future navigateToinfo(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => info()));
+  }
+  Future navigateToplants(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => plants()));
+  }
+  Future navigateTosettings(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => settings()));
+  }
+  Future navigateToconnect(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => connect()));
   }
 }
