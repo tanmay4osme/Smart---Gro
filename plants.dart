21c7f0e51aad132c6ab1482f671dcf158dedbbcb
Adit Bala <62257689+adit-bala@users.noreply.github.com>
2020-03-29 14:01:39 -0400

    Update plants.dart

--- plants.dart
+++ plants.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
 
-class plants extends StatelessWidget {
+class TableLayout extends StatefulWidget {
+  @override
+  _TableLayoutState createState() => _TableLayoutState();
+}
+class _TableLayoutState extends State<TableLayout> {
+  List<List<dynamic>> data = [];
+
+  loadAsset() async {
+    final myData = await rootBundle.loadString('assets/plantInfo-clean.csv');
+    print(myData);
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(myData);
+    data = csvTable;
+  }
+
+  //List<String> plantNames = data.getRange(0, data.length);
+  List<String> descriptions;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          onPressed: () async {
+            await loadAsset();
+            print(data);
+          }),
       appBar: AppBar(
-        title: Text('Sub Page 2'),
-        backgroundColor: Colors.purpleAccent,
+        title: Text("Plants!"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Click button to back to Main Page'),
-            RaisedButton(
-              textColor: Colors.black,
-              color: Colors.green[200],
-              child: Text('Back to Main Page'),
-              onPressed: () {
-                backToMainPage(context);
-              },
-            )
-          ],
-        ),
+      body: Table(
+        children: null
       ),
     );
   }
-
+}
   void backToMainPage(context) {
     Navigator.pop(context);
   }
-}
