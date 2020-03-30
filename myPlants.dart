955bfc15939d5921a557c09599d9f20b6a68a3ba
Adit Bala <adityarbala@gmail.com>
2020-03-29 21:29:18 -0400

    Update myPlants.dart

--- myPlants.dart
+++ myPlants.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
-import 'package:flutter/material.dart';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
-import 'plants.dart';
 
 class TableLayout1 extends StatefulWidget {
   @override
-  MyPlants createState() => MyPlants(null);
+  MyPlants createState() => MyPlants();
 }
 
 class MyPlants extends State<TableLayout1> {
   List<List<dynamic>> data = [];
-  List<String> userPlants = new List<String>();
+  List<List<String>> userPlants = new List<List<String>>();
+  List<String> descriptions = new List<String>();
 
-  MyPlants(String user) {
+  MyPlants({List user}) {
     userPlants.add(user);
   }
   loadAsset() async {
@@ -22,7 +21,8 @@ class MyPlants extends State<TableLayout1> {
     print(myData);
     List<List<dynamic>> csvTable = CsvToListConverter().convert(myData);
     data = csvTable;
-}
+  }
+
   @override
   Widget build(BuildContext context) {
     loadAsset();
@@ -30,45 +30,45 @@ class MyPlants extends State<TableLayout1> {
         appBar: AppBar(
           title: Text('Flutter Tutorial - TutorialKart'),
         ),
-        body: ListView(children: <Widget>[
-          Center(
-              child: Text(
-                'Students',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              )),
-          DataTable(
-            columns: [
-              DataColumn(label: Text('Name')),
-              DataColumn(label: Text('AlternateName')),
-              DataColumn(label: Text('sowinstructions')),
-              DataColumn(label: Text('spaceinstructions')),
-              DataColumn(label: Text('harvestinstructions')),
-              DataColumn(label: Text('compatibleplants')),
-              DataColumn(label: Text('avoidinstructions')),
-              DataColumn(label: Text('culinaryhints')),
-              DataColumn(label: Text('culinarypreservation')),
-            ],
-            rows: [
-              DataRow(cells: [
-                DataCell(Text('1')),
-                DataCell(Text('Arya')),
-                DataCell(Text('6')),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('12')),
-                DataCell(Text('John')),
-                DataCell(Text('9')),
-              ]),
-              DataRow(cells: [
-                DataCell(Text('42')),
-                DataCell(Text('Tony')),
-                DataCell(Text('8')),
-              ]),
-            ],
-          ),
-        ],
-    )
-    );
+        body: ListView(
+          children: <Widget>[
+            Center(
+                child: Text(
+              'Students',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )),
+            DataTable(
+              columns: [
+                DataColumn(label: Text('Name')),
+                DataColumn(label: Text('AlternateName')),
+                DataColumn(label: Text('sowinstructions')),
+                DataColumn(label: Text('spaceinstructions')),
+                DataColumn(label: Text('harvestinstructions')),
+                DataColumn(label: Text('compatibleplants')),
+                DataColumn(label: Text('avoidinstructions')),
+                DataColumn(label: Text('culinaryhints')),
+                DataColumn(label: Text('culinarypreservation')),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('1')),
+                  DataCell(Text('Arya')),
+                  DataCell(Text('6')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('12')),
+                  DataCell(Text('John')),
+                  DataCell(Text('9')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('42')),
+                  DataCell(Text('Tony')),
+                  DataCell(Text('8')),
+                ]),
+              ],
+            ),
+          ],
+        ));
   }
 }
 
